fcfa998d8ffdfdad4a409a770e78e58e74416fe1
keep_moving <397091336@qq.com>
2021-06-09 23:55:17 +0800

    'rest_with_actuator'

---
+++ rest_with_actuator/src/main/java/com/example/rest_with_actuator/RestWithActuatorApplication.java
@@ -0,0 +1,13 @@
+package com.example.rest_with_actuator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestWithActuatorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestWithActuatorApplication.class, args);
+    }
+
+}
