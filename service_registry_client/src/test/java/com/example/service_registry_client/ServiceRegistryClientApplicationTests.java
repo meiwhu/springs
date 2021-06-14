a0809ec7b2eac81fdea94134189b60d0b1c781ed
keep_moving <397091336@qq.com>
2021-06-14 20:22:09 +0800

    service-registry-and-discovery

---
+++ service_registry_client/src/test/java/com/example/service_registry_client/ServiceRegistryClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.service_registry_client;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceRegistryClientApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ service_registry_server/src/main/java/com/example/service_registry_server/ServiceRegistryServerApplication.java
@@ -0,0 +1,15 @@
+package com.example.service_registry_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@EnableEurekaServer
+@SpringBootApplication
+public class ServiceRegistryServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServiceRegistryServerApplication.class, args);
+    }
+
+}
