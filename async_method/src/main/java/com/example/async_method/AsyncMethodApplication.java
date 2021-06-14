8e1875fd1bc58e87ef5680e9513dc69585107f87
keep_moving <397091336@qq.com>
2021-06-14 16:38:16 +0800

    async-method

---
+++ async_method/src/main/java/com/example/async_method/AsyncMethodApplication.java
@@ -0,0 +1,31 @@
+package com.example.async_method;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.task.TaskExecutor;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
+import java.util.concurrent.Executor;
+import java.util.concurrent.ThreadPoolExecutor;
+
+@SpringBootApplication
+@EnableAsync
+public class AsyncMethodApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AsyncMethodApplication.class, args).close();
+    }
+
+    @Bean
+    public TaskExecutor taskExecutor() {
+        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+        executor.setCorePoolSize(2);
+        executor.setMaxPoolSize(2);
+        executor.setQueueCapacity(500);
+        executor.setThreadNamePrefix("GithubLookup-");
+        executor.initialize();
+        return executor;
+    }
+}
---
+++ async_method/src/main/java/com/example/async_method/User.java
@@ -0,0 +1,33 @@
+package com.example.async_method;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class User {
+    String name;
+    String blog;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getBlog() {
+        return blog;
+    }
+
+    public void setBlog(String blog) {
+        this.blog = blog;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "name='" + name + '\'' +
+                ", blog='" + blog + '\'' +
+                '}';
+    }
+}
